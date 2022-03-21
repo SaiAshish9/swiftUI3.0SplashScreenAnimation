10fe8626d3405a9f8431401915486541a72bca1c
saiashish9 <saiashish3760@gmail.com>
2022-03-21 15:58:26 +0530

    initial commit

---
+++ animatedSplashScreen/animatedSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  animatedSplashScreenApp.swift
+//  animatedSplashScreen
+//
+//  Created by Sai Ashish Darapureddy on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct animatedSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
